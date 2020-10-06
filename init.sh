8fee6c883d9dfb86e7fd4478eaba7bc06d56ba04
Guangrui Wang <aguang.xyz@gmail.com>
2020-10-06 14:23:08 +1300

    Reformat.

--- init.sh
+++ init.sh
@@ -8,8 +8,7 @@ NVM_DATA_DIR="${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site
 mkdir -p $NVM_DATA_DIR
 
 # 2.2 Create soft link of `autoload` if not exists.
-if [ ! -d "$NVM_DATA_DIR/autoload" ]
-then
+if [ ! -d "$NVM_DATA_DIR/autoload" ]; then
   ln -s $PWD/autoload ${NVM_DATA_DIR}/autoload
 else
   echo "[WARN] Directory \`$NVM_DATA_DIR/autoload\` already exists."
@@ -22,8 +21,7 @@ NVM_CONF_DIR="$HOME/.config/nvim"
 mkdir -p $NVM_CONF_DIR
 
 # 3.2 Create soft link of `init.vim` if not exists.
-if [ ! -f "$NVM_CONF_DIR/init.vim" ]
-then
+if [ ! -f "$NVM_CONF_DIR/init.vim" ]; then
   ln -s $PWD/init.vim $NVM_CONF_DIR/init.vim
 else
   echo "[WARN] Directory \`$NVM_CONF_DIR/init.vim\` already exists."
@@ -45,3 +43,9 @@ sudo apt install -y pandoc
 
 # 4.5 Install [clang-format](http://clang.llvm.org/docs/ClangFormat.html).
 sudo apt install -y clang-format
+
+# 4.6 Install [astyle](http://astyle.sourceforge.net/).
+sudo apt install astyle
+
+# 4.7 Install [shfmt](https://snapcraft.io/install/shfmt/ubuntu).
+sudo snap install shfmt
